bfbffc0d8e693c4d56c5128123c6250d2adfed3a
Turk8i <alamriturki00@outlook.com>
2025-06-18 13:32:21 +0300

    Limiting FPS to 60

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    clock = pygame.time.Clock()
+    deltaTime = 0
+
+
     
     while True:
 
@@ -20,9 +24,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill((0,0,0))  # fill method for surface that fills the surface color to *black*
+        screen.fill("black")  # fill method for surface that fills the surface color to *black*
         pygame.display.flip() # display.flip() which refreshes the screen
-
+        x = clock.tick(60) # Pausing the game until 1/60 seconds has passed
+        dt = x / 1000 # converting from milisecond to seconds
 
 
 if __name__ == "__main__":
